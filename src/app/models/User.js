c05420c2f561d2176891813b95b86f28d478e2b1
Guilherme Beidaki Peres <guilherme.peres@tecnospeed.com.br>
2019-10-11 08:44:11 -0300

    password crypt hash

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcryptjs'
 
 export class User extends Model {
   static init (sequelize) {
@@ -6,6 +7,7 @@ export class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN
       },
@@ -13,5 +15,13 @@ export class User extends Model {
         sequelize
       }
     )
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8)
+      }
+    })
+
+    return this
   }
 }
