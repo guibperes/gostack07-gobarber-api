4f30aae93f47dfb497fefed2e497fdee8fbe0b5a
Guilherme Beidaki Peres <guilherme.peres@tecnospeed.com.br>
2019-10-11 10:50:27 -0300

    body validation

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,22 @@
 import jwt from 'jsonwebtoken'
+import * as Yup from 'yup'
 
 import { AUTH_SECRET, EXPIRES_IN } from '../../config/auth'
 import { User } from '../models/User'
 
 export class SessionController {
   async store (req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string().email().required(),
+      password: Yup.string().required()
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        message: 'Validation failed'
+      })
+    }
+
     const { email, password } = req.body
 
     const user = await User.findOne({ where: { email } })
