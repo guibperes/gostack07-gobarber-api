c8823d05cde6445e26bac97721eb755cbf5a9620
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-10-13 18:04:32 -0300

    listed all apponitments of the day

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,50 @@
+import { Op } from 'sequelize'
+import { startOfDay, endOfDay, parseISO } from 'date-fns'
+
+import { Appointment } from '../models/Appointment'
+import { User } from '../models/User'
+import { File } from '../models/File'
+
+export class ScheduleController {
+  async index (req, res) {
+    const { date } = req.query
+
+    const parsedDate = parseISO(date)
+
+    const isProvider = await User.findOne({
+      where: { id: req.user, provider: true }
+    })
+
+    if (!isProvider) {
+      return res.status(401).json({ message: 'User is not a provider' })
+    }
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.user,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)]
+        }
+      },
+      attributes: ['id', 'date'],
+      order: ['date'],
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['name', 'path', 'url']
+            }
+          ]
+        }
+      ]
+    })
+
+    return res.json(appointments)
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import { SessionController } from './app/controllers/SessionController'
 import { FileController } from './app/controllers/FileController'
 import { ProviderController } from './app/controllers/ProviderController'
 import { AppointmentController } from './app/controllers/AppointmentController'
+import { ScheduleController } from './app/controllers/ScheduleController'
 
 const routes = new Router()
 
@@ -15,6 +16,7 @@ const sessionController = new SessionController()
 const fileController = new FileController()
 const providerController = new ProviderController()
 const appointmentController = new AppointmentController()
+const scheduleController = new ScheduleController()
 
 // PUBLIC ROUTES
 routes.post('/users', userController.store)
@@ -27,5 +29,6 @@ routes.post('/files', upload.single('file'), fileController.store)
 routes.get('/providers', providerController.index)
 routes.post('/appointments', appointmentController.store)
 routes.get('/appointments', appointmentController.index)
+routes.get('/schedules', scheduleController.index)
 
 export default routes
