c390ad0a86e624f85164d24236158e041a684854
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-10-13 17:02:03 -0300

    date is valid check and check availability

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup'
+import { startOfHour, parseISO, isBefore } from 'date-fns'
 
 import { Appointment } from '../models/Appointment'
 import { User } from '../models/User'
@@ -23,8 +24,27 @@ export class AppointmentController {
     })
 
     if (!isProvider) {
-      return res.status(401)
+      return res
+        .status(401)
         .json({ message: 'You can only create appointments with providers' })
+    }
+
+    const hourStart = startOfHour(parseISO(date))
+
+    if (isBefore(hourStart, Date.now())) {
+      return res.status(400).json({ message: 'Past dates are not permited' })
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart
+      }
+    })
+
+    if (checkAvailability) {
+      return res.status(400).json({ message: 'Appointment date is not available' })
     }
 
     const appointment = await Appointment.create({
