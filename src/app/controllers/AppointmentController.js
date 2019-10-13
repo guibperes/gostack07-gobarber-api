b9539b102b3a871b2fc2485c8bc8432071bfc61b
Guilherme Beidaki Peres <guibperes@gmail.com>
2019-10-13 17:26:23 -0300

    created pagination on appointments list

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -58,6 +58,8 @@ export class AppointmentController {
   }
 
   async index (req, res) {
+    const { page = 1 } = req.query
+
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.user,
@@ -65,6 +67,8 @@ export class AppointmentController {
       },
       attributes: ['id', 'date'],
       order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
